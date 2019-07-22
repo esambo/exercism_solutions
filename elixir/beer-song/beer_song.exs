bdc43d63f44b2980c56f1ac5007578fb2f9a948d
Emmanuel Sambo <news@EmmanuelSambo.com>
2019-07-22 10:35:08 -0500

    Elixir: Beer Song

---
+++ elixir/beer-song/beer_song.exs
@@ -0,0 +1,47 @@
+defmodule BeerSong do
+  @doc """
+  Get a single verse of the beer song
+  """
+  @spec verse(integer) :: String.t()
+  def verse(0) do
+    ~S"""
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
+  def verse(1) do
+    ~S"""
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+
+  def verse(2) do
+    ~S"""
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+
+  def verse(number) do
+    ~s"""
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    """
+  end
+
+  @doc """
+  Get the entire beer song for a given range of numbers of bottles.
+  """
+  @spec lyrics(Range.t()) :: String.t()
+  def lyrics(range \\ 99..0)
+
+  def lyrics(0..0) do
+    verse(0)
+  end
+
+  def lyrics(range) do
+    verse(range.first) <> "\n" <> lyrics((range.first - 1)..range.last)
+  end
+end
