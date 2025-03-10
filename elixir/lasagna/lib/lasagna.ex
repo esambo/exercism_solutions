acfc9b593cdaf96295beeb7dfa2fce485a71c848
Emmanuel Sambo <news@EmmanuelSambo.com>
2025-03-10 18:39:58 -0500

    implement

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,11 +1,21 @@
 defmodule Lasagna do
-  # Please define the 'expected_minutes_in_oven/0' function
+  def expected_minutes_in_oven do
+    40
+  end
 
-  # Please define the 'remaining_minutes_in_oven/1' function
+  def remaining_minutes_in_oven(actual) do
+    expected_minutes_in_oven() - actual
+  end
 
-  # Please define the 'preparation_time_in_minutes/1' function
+  def preparation_time_in_minutes(layers) do
+    layers * 2
+  end
 
-  # Please define the 'total_time_in_minutes/2' function
+  def total_time_in_minutes(layers, actual) do
+    preparation_time_in_minutes(layers) + actual
+  end
 
-  # Please define the 'alarm/0' function
+  def alarm do
+    "Ding!"
+  end
 end
