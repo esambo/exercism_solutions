df5cefff23ebfe953d20ff1265733fd2ebd34c22
Emmanuel Sambo <news@EmmanuelSambo.com>
2026-01-01 12:32:15 -0600

    implementation

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,17 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    hourly_rate * 8.0
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount * (1 - discount / 100)
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    daily_rate(hourly_rate) * 22 |> apply_discount(discount) |> Float.ceil() |> trunc()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    budget / (daily_rate(hourly_rate) |> apply_discount(discount)) |> Float.floor(1)
   end
 end
