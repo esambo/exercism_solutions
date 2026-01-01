4935bb9454b4a334e5926a977f9850da1e79d7d3
Emmanuel Sambo <news@EmmanuelSambo.com>
2026-01-01 15:56:41 -0600

    Refactor: extract conversion table

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,46 +1,22 @@
 defmodule KitchenCalculator do
+  @conversion %{
+    milliliter: 1,
+    cup: 240,
+    fluid_ounce: 30,
+    teaspoon: 5,
+    tablespoon: 15
+  }
+
   def get_volume(volume_pair) do
     elem(volume_pair, 1)
   end
 
-  def to_milliliter({:milliliter, volume}) do
-    {:milliliter, volume}
+  def to_milliliter({unit, volume}) do
+    {:milliliter, volume * @conversion[unit]}
   end
 
-  def to_milliliter({:tablespoon, volume}) do
-    {:milliliter, volume * 15}
-  end
-
-  def to_milliliter({:cup, volume}) do
-    {:milliliter, volume * 240}
-  end
-
-  def to_milliliter({:fluid_ounce, volume}) do
-    {:milliliter, volume * 30}
-  end
-
-  def to_milliliter({:teaspoon, volume}) do
-    {:milliliter, volume * 5}
-  end
-
-  def from_milliliter({:milliliter, volume}, :milliliter) do
-    {:milliliter, volume}
-  end
-
-  def from_milliliter({:milliliter, volume}, :cup) do
-    {:cup, volume / 240}
-  end
-
-  def from_milliliter({:milliliter, volume}, :fluid_ounce) do
-    {:fluid_ounce, volume / 30}
-  end
-
-  def from_milliliter({:milliliter, volume}, :teaspoon) do
-    {:teaspoon, volume / 5}
-  end
-
-  def from_milliliter({:milliliter, volume}, :tablespoon) do
-    {:tablespoon, volume / 15}
+  def from_milliliter({:milliliter, volume}, unit) do
+    {unit, volume / @conversion[unit]}
   end
 
   def convert(volume_pair, unit) do
