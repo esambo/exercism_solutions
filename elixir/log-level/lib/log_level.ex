4e20ebb39a14a2df48cad5ce56ca6a8a86ae425e
Emmanuel Sambo <news@EmmanuelSambo.com>
2026-01-01 14:48:12 -0600

    implementation

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,9 +1,23 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
     # Please implement the to_label/2 function
+    cond do
+      level == 0 and not legacy? -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 and not legacy? -> :fatal
+      true -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    case to_label(level, legacy?) do
+      :error -> :ops
+      :fatal -> :ops
+      :unknown -> if legacy?, do: :dev1, else: :dev2
+      _ -> false
+    end
   end
 end
