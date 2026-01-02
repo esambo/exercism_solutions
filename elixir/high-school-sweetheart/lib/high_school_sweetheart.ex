47f3e471ad0bbc7da9b19a8bb8307cb9b08abeb6
Emmanuel Sambo <news@EmmanuelSambo.com>
2026-01-02 11:42:37 -0600

    implementation

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,21 +1,23 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
-    # Please implement the first_letter/1 function
+    name |> String.trim() |> String.first()
   end
 
   def initial(name) do
-    # Please implement the initial/1 function
+    name |> first_letter() |> String.upcase() |> Kernel.<>(".")
   end
 
   def initials(full_name) do
-    # Please implement the initials/1 function
+    full_name |> String.trim() |> String.split() |> Enum.map_join(" ", &(initial(&1)))
   end
 
   def pair(full_name1, full_name2) do
-    # ❤-------------------❤
-    # |  X. X.  +  X. X.  |
-    # ❤-------------------❤
-
-    # Please implement the pair/2 function
+    initials1 = initials(full_name1)
+    initials2 = initials(full_name2)
+    """
+    ❤-------------------❤
+    |  #{initials1}  +  #{initials2}  |
+    ❤-------------------❤
+    """
   end
 end
