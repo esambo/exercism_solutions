a63a775ebdfe5ae00c7c026277b41d66133715e2
Emmanuel Sambo <news@EmmanuelSambo.com>
2026-01-02 12:55:43 -0600

    implementation

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,21 +1,55 @@
 defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
+  def today([]) do
+    nil
   end
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
+  def today([today | _rest]) do
+    today
   end
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
+  def increment_day_count([]) do
+    [1]
   end
 
-  def total(list) do
-    # Please implement the total/1 function
+  def increment_day_count([today | rest]) do
+    [today + 1 | rest]
   end
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
+  def has_day_without_birds?(list, has_birds \\ false)
+
+  def has_day_without_birds?([], false) do
+    false
+  end
+
+  def has_day_without_birds?([0 | _rest], _false) do
+    true
+  end
+
+  def has_day_without_birds?([_today | rest], _false) do
+    has_day_without_birds?(rest, false)
+  end
+
+  def total(list, total \\ 0)
+
+  def total([], total) do
+    total
+  end
+
+  def total([today | rest], total) do
+    total(rest, total + today)
+  end
+
+  def busy_days(list, days \\ 0)
+
+  def busy_days([], days) do
+    days
+  end
+
+  def busy_days([today | rest], days) when today < 5 do
+    busy_days(rest, days)
+  end
+
+  def busy_days([_today | rest], days) do
+    busy_days(rest, days + 1)
   end
 end
