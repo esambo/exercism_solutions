0f416af49ba86c5b8ae3726991e28e8c7df473d6
Emmanuel Sambo <news@EmmanuelSambo.com>
2026-03-15 21:01:42 -0500

    Implement Binary Search

--- elixir/binary-search/lib/binary_search.ex
+++ elixir/binary-search/lib/binary_search.ex
@@ -14,10 +14,34 @@ defmodule BinarySearch do
 
       iex> BinarySearch.search({1, 3, 5}, 5)
       {:ok, 2}
-
   """
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
   def search(numbers, key) do
+    [left, right] = [0, tuple_size(numbers) - 1]
+    search(numbers, key, left, right)
+  end
+
+  defp search(numbers, key, left, right) do
+    index = left + div(right - left, 2)
+    search(numbers, key, left, right, index)
+  end
+
+  defp search(_numbers, _key, left, right, _index) when left > right do
+    :not_found
+  end
+
+  defp search(numbers, key, _left, _right, index) when elem(numbers, index) == key do
+    {:ok, index}
+  end
+
+  defp search(numbers, key, _left, right, index) when elem(numbers, index) < key do
+    left = index + 1
+    search(numbers, key, left, right)
+  end
+
+  defp search(numbers, key, left, _right, index) do
+    right = index - 1
+    search(numbers, key, left, right)
   end
 end
