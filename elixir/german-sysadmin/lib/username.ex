d0be7dd5ca7736665849d50f2e94689b6c913294
Emmanuel Sambo <news@EmmanuelSambo.com>
2026-03-23 17:05:15 -0500

    implement

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,10 +1,15 @@
 defmodule Username do
   def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
-
-    # Please implement the sanitize/1 function
+    Enum.flat_map(username, fn char ->
+      case char do
+        lower_case when lower_case in ?a..?z -> [lower_case]
+        ?_ -> [?_]
+        ?ä -> ~c"ae"
+        ?ö -> ~c"oe"
+        ?ü -> ~c"ue"
+        ?ß -> ~c"ss"
+        _disallowed -> []
+      end
+    end)
   end
 end
